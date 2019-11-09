34b0d4534ca435f0ec589f8d3090fe7feb471bca
jaumevr15 <53271315+jaumevr15@users.noreply.github.com>
2019-11-09 21:16:26 +0100

    Challenge 2 done

--- your-code/challenge-2.py
+++ your-code/challenge-2.py
@@ -6,33 +6,38 @@ the strings being generated.
 The code is functional but has a lot of room for improvement. Use what you have learned
 about simple and efficient code, refactor the code.
 """
+#Importing all libraries at once
+import random
+import string
+import sys
 
-def RandomStringGenerator(l=12, a=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9']):
+#Simplified the list of characters by using string methods.
+char = [string.ascii_lowercase + string.digits]
+wordlist = [ch for ch in char]
+
+def Rand_Str_Gen(l=12, a=wordlist):
     p = 0
-    s = ''
+    string = ''
     while p<l:
-        import random
-        s += random.choice(a)
+        string += random.choice(a)
         p += 1
-    return s
+    return string
 
-def BatchStringGenerator(n, a=8, b=12):
-    r = []
-    for i in range(n):
-        c = None
-        if a < b:
-            import random
-            c = random.choice(range(a, b))
-        elif a == b:
-            c = a
+def Batch_Str_Gen(n, min_len=8, max_len=12):
+    str_list = []
+    for r in range(n):
+        result = None
+        if min_len < max_len:
+            result = random.choice(range(min_len, max_len))
+        elif min_len == max_len:
+            result = min3_len
         else:
-            import sys
             sys.exit('Incorrect min and max string lengths. Try again.')
-        r.append(RandomStringGenerator(c))
-    return r
+        str_list.append(Rand_Str_Gen(result))
+    return str_list
 
-a = input('Enter minimum string length: ')
-b = input('Enter maximum string length: ')
-n = input('How many random strings to generate? ')
+min_len = input('Enter minimum string length: ')
+max_len = input('Enter maximum string length: ')
+num_str = input('How many random strings to generate? ')
 
-print(BatchStringGenerator(int(n), int(a), int(b)))
+print(Batch_Str_Gen(int(num_str), int(min_len), int(max_len)))
